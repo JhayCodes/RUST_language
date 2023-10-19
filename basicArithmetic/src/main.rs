084d4dd14e7bc4968a56df4b0f88f7435966bb15
JhayCodes <jhayycodes@gmail.com>
2023-10-19 14:53:16 +0000

    Some basic arithmetic

---
+++ basicArithmetic/src/main.rs
@@ -0,0 +1,19 @@
+fn sum(a:i32, b:i32) -> i32{
+    a + b;
+}
+
+fn sub(a:i32, b:i32) -> i32{
+    a - b;
+}
+
+fn div(a:i32, b:i32) -> i32{
+    a/b;
+}
+
+fn main() {
+    let sum = 2 + 2; //4
+    let value = 10 - 5; //5
+    let division = 10/2; //8
+    let mult = 5 * 5; //25
+    let modulus = 5 % 2; //1
+}
