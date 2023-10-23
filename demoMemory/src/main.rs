80f8bf3d370ae8d35d676fc9393d28faa27584a7
JhayCodes <jhayycodes@gmail.com>
2023-10-23 11:22:03 +0000

    basic knowledge of memory concept

---
+++ demoMemory/src/main.rs
@@ -0,0 +1,27 @@
+// Basic memeory refresh
+// *Memory is stored using binary
+//     Bits: 0 or 1
+// *Computers are optimized for bytes
+//     1 byte == 8 contiguous Bits
+
+// Addresses
+// *All data in memory has an "address"
+//     used to locate data
+//     always the same - only data changes
+// *Usually don't utilize addresses directly
+//     Variables handle most of the work
+
+// Offsets
+// *Are used to locate items located at an address
+// *It begins from 0
+// *It represent the number of bytes away from the original address
+//     deals with indexes
+
+// Data[1]
+// Data == address
+// [1] == offset of 1
+
+
+fn main() {
+    println!("All about Memory");
+}
