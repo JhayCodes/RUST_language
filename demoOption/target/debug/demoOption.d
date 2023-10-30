61e1a5672b9acb84438bc0d09df7e3a8923703cc
JhayCodes <jhayycodes@gmail.com>
2023-10-30 23:07:44 +0000

    added notes on options to demo

---
+++ demoOption/target/debug/demoOption.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\demoOption\target\debug\demoOption.exe: D:\ProgramPractice\RUST_language\demoOption\src\main.rs
---
+++ demoOption/target/debug/deps/demoOption.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\demoOption\target\debug\deps\demoOption.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\demoOption\target\debug\deps\demoOption.d: src\main.rs
+
+src\main.rs:
