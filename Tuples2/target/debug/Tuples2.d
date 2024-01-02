c7d3ccbce0d79663a43db13dc8fda763f060513e
JhayCodes <jhayycodes@gmail.com>
2024-01-02 22:39:50 +0000

    Added some configuration files and tuple2, containing example of indexing a tuple

---
+++ Tuples2/target/debug/Tuples2.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\Tuples2\target\debug\Tuples2.exe: D:\ProgramPractice\RUST_language\Tuples2\src\main.rs
---
+++ Tuples2/target/debug/deps/Tuples2.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\Tuples2\target\debug\deps\Tuples2.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\Tuples2\target\debug\deps\Tuples2.d: src\main.rs
+
+src\main.rs:
