a45e7145957258ab4a34d4e8a812e621aabd9362
JhayCodes <jhayycodes@gmail.com>
2024-01-06 21:52:00 +0000

    Created a car and some instances with a struct and enum

---
+++ Car/target/debug/Car.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\Car\target\debug\Car.exe: D:\ProgramPractice\RUST_language\Car\src\main.rs
---
+++ Car/target/debug/deps/Car.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\Car\target\debug\deps\Car.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\Car\target\debug\deps\Car.d: src\main.rs
+
+src\main.rs:
