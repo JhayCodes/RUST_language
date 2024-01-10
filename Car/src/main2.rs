df0a2e4fb01eb475ef22d88be546209b803288a6
JhayCodes <jhayycodes@gmail.com>
2024-01-10 13:54:18 +0000

    created an array of colors for the car, created other instances of car

--- Car/src/main2.rs
+++ Car/src/main2.rs
@@ -25,7 +25,7 @@ fn car_quality (miles: u32) -> (Age, u32) {
 
     // Declare and initialize the return tuple value
     // For a new car, set the miles to 0
-    let quality: (Age, u32) = (Age::New, 0);
+    let quality: (Age, u32) = (Age::New, miles);
 
     // Return the completed tuple to the caller
     quality
@@ -37,11 +37,38 @@ struct Car {
     color: String,
     motor: Transmission,
     roof: bool,
-    age: Age
+    age: (Age, u32)
 }
 
+#[derive(PartialEq, Debug)]
 enum Age { New, Used}
 
 #[derive(PartialEq, Debug)]
 // Declare enum for Car transmission type
 enum Transmission { Manual, SemiAuto, Automatic }
+
+fn main() {
+    // Create car color array
+    let colors = ["Blue", "Green", "Red","Silver"];
+    // Declare the car type and initial values
+
+    let mut car = Car;
+
+    let mut engine = Transmission::Manual; 
+    // Order 3 cars, one car for each type of transmission
+
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(String::from(colors[0]), engine, true, 0);
+    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1); 
+
+}  
