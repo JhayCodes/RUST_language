b1783fcd90a8ff336b2a63f6522b099aa24c2267
JhayCodes <jhayycodes@gmail.com>
2024-01-11 20:30:00 +0000

    contains codes in main2.rs in Car directory

---
+++ Car2/target/debug/Car2.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\Car2\target\debug\Car2.exe: D:\ProgramPractice\RUST_language\Car2\src\main.rs
---
+++ Car2/target/debug/deps/Car2.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\Car2\target\debug\deps\Car2.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\Car2\target\debug\deps\Car2.d: src\main.rs
+
+src\main.rs:
