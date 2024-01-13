ad995c3f9483bfdc08fcef1e2e3e8b7352c6525d
JhayCodes <jhayycodes@gmail.com>
2024-01-13 21:10:49 +0000

    How if Else is used in Rust

---
+++ ifElse/target/debug/deps/ifElse.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\ifElse\target\debug\deps\ifElse.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\ifElse\target\debug\deps\ifElse.d: src\main.rs
+
+src\main.rs:
---
+++ ifElse/target/debug/ifElse.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\ifElse\target\debug\ifElse.exe: D:\ProgramPractice\RUST_language\ifElse\src\main.rs
