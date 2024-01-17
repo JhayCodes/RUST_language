a2ec116d7c2a93a121be4d702c64837e04a58aa1
JhayCodes <jhayycodes@gmail.com>
2024-01-17 20:45:41 +0000

    added a hash map to track car orders

---
+++ Car3/target/debug/Car3.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\Car3\target\debug\Car3.exe: D:\ProgramPractice\RUST_language\Car3\src\main.rs
---
+++ Car3/target/debug/deps/Car3.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\Car3\target\debug\deps\Car3.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\Car3\target\debug\deps\Car3.d: src\main.rs
+
+src\main.rs:
