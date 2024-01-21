f217a8c5349a0ab59ebd44bbb8b12ba7c533bdbc
JhayCodes <jhayycodes@gmail.com>
2024-01-21 21:32:14 +0000

    error handling using option enum

---
+++ ErrorHandling/target/debug/ErrorHandling.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\ErrorHandling\target\debug\ErrorHandling.exe: D:\ProgramPractice\RUST_language\ErrorHandling\src\main.rs
---
+++ ErrorHandling/target/debug/deps/ErrorHandling.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\ErrorHandling\target\debug\deps\ErrorHandling.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\ErrorHandling\target\debug\deps\ErrorHandling.d: src\main.rs
+
+src\main.rs:
