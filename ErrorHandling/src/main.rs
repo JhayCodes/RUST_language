a727327a2ee2f512b269b114d2c5cc101c1302f3
JhayCodes <jhayycodes@gmail.com>
2024-01-22 22:30:05 +0000

    Added an if let expression

--- ErrorHandling/src/main.rs
+++ ErrorHandling/src/main.rs
@@ -3,8 +3,20 @@ fn main() {
 
     for &index in [0, 2, 99].iter() {
         match fruits.get(index) {
+            Some(&"coconut") => println!("Coconuts are awesome!!!"),
             Some(fruit_name) => println!("It's a delicious {}!", fruit_name),
             None => println!("There is no fruit! :("),
         }
     }
+
+    let a_number: Option<u8> = Some(7);
+    if let Some(7) => a_number{
+        println!("That's my lucky number!");
+    }
+
+    // match a_number {
+    //     Some(7) => println!("That's my lucky number!"),
+    //     _ => {},
+    // }
+
 }
