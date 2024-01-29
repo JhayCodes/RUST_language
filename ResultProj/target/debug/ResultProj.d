799f5fab3f4d4d20e8627aadb554938ca70ed42a
JhayCodes <jhayycodes@gmail.com>
2024-01-29 14:34:16 +0000

    implemented codes at the TODO macro

---
+++ ResultProj/target/debug/ResultProj.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\ResultProj\target\debug\ResultProj.exe: D:\ProgramPractice\RUST_language\ResultProj\src\main.rs
---
+++ ResultProj/target/debug/deps/ResultProj.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\ResultProj\target\debug\deps\ResultProj.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\ResultProj\target\debug\deps\ResultProj.d: src\main.rs
+
+src\main.rs:
