90bf02876dbada82f107d3c2ab59b15bba661e5e
JhayCodes <jhayycodes@gmail.com>
2024-02-04 14:43:06 +0000

    initial commit

---
+++ LifetimesProj/target/debug/LifetimesProj.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\LifetimesProj\target\debug\LifetimesProj.exe: D:\ProgramPractice\RUST_language\LifetimesProj\src\main.rs
---
+++ LifetimesProj/target/debug/deps/LifetimesProj.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\LifetimesProj\target\debug\deps\LifetimesProj.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\LifetimesProj\target\debug\deps\LifetimesProj.d: src\main.rs
+
+src\main.rs:
