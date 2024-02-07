9371365c1d58c6979d18bc52055f44c94fafe5d0
JhayCodes <jhayycodes@gmail.com>
2024-02-07 21:50:08 +0000

    example of how trait is used in Rust

---
+++ Traits/target/debug/Traits.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\Traits\target\debug\Traits.exe: D:\ProgramPractice\RUST_language\Traits\src\main.rs
---
+++ Traits/target/debug/deps/Traits.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\Traits\target\debug\deps\Traits.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\Traits\target\debug\deps\Traits.d: src\main.rs
+
+src\main.rs:
