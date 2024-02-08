97b3c89a7b89db1bf64e928ee54a5978e0525c6d
JhayCodes <jhayycodes@gmail.com>
2024-02-08 19:08:42 +0000

    explicitly implemented the Display triat

---
+++ DeriveTrait/target/debug/DeriveTrait.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\DeriveTrait\target\debug\DeriveTrait.exe: D:\ProgramPractice\RUST_language\DeriveTrait\src\main.rs
---
+++ DeriveTrait/target/debug/deps/DeriveTrait.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\DeriveTrait\target\debug\deps\DeriveTrait.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\DeriveTrait\target\debug\deps\DeriveTrait.d: src\main.rs
+
+src\main.rs:
