abdf5acfe14f74b385b57ff20e71827021b4c871
JhayCodes <jhayycodes@gmail.com>
2024-02-19 22:10:28 +0000

    implemented our own iterator

---
+++ TraitIterator/target/debug/TraitIterator.d
@@ -0,0 +1,1 @@
+D:\ProgramPractice\RUST_language\TraitIterator\target\debug\TraitIterator.exe: D:\ProgramPractice\RUST_language\TraitIterator\src\main.rs
---
+++ TraitIterator/target/debug/deps/TraitIterator.d
@@ -0,0 +1,5 @@
+D:\ProgramPractice\RUST_language\TraitIterator\target\debug\deps\TraitIterator.exe: src\main.rs
+
+D:\ProgramPractice\RUST_language\TraitIterator\target\debug\deps\TraitIterator.d: src\main.rs
+
+src\main.rs:
